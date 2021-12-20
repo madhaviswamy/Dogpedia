081aa4d31450172bb7c00bb2c3e67e85a661d85e
Madhavi Swamy <47503135+madhaviswamy@users.noreply.github.com>
2021-12-20 08:31:09 -0800

    Updating JS code
    Adding class to JS file and using DOM EventListeners

--- slide.js
+++ slide.js
@@ -1,37 +1,83 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+function getSliderString(firstImage, imagesLength) {
+  return `<div class="container">
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+      <div>
+      <span class="box">
+        <img src="${firstImage}"></img>
+        <div class="caption">1/${imagesLength}</div>
+      </span>
+    </div>
+
+      <button disabled class="prev">&#10094;</button>
+      <button class="next">&#10095;</button>
+    </div>`;
 }
 
-function showSlides(nextSlide) {
-  const slides = document.getElementsByClassName("blogSlides");
+class Slider {
+  images = [];
+  sliderRoot = null;
+  slideIndex = 1;
 
-  console.log({ slideIndex });
+  showSlides(nextSlide) {
+    if (this.slideIndex === images.length) {
+      document.querySelector(`${this.sliderRoot} .next`).disabled = true;
+    } else {
+      document.querySelector(`${this.sliderRoot} .next`).disabled = false;
+    }
 
-  if (slideIndex === slides.length) {
-    document.getElementById("next").disabled = true;
-  } else {
-    document.getElementById("next").disabled = false;
+    if (this.slideIndex === 1) {
+      document.querySelector(`${this.sliderRoot} .prev`).disabled = true;
+    } else {
+      document.querySelector(`${this.sliderRoot} .prev`).disabled = false;
+    }
+
+    if (nextSlide > images.length) {
+      this.slideIndex = 1;
+    }
+    if (nextSlide < 1) {
+      this.slideIndex = images.length;
+    }
+
+    document
+      .querySelector(`${this.sliderRoot} img`)
+      .setAttribute("src", images[this.slideIndex - 1]);
+
+    document.querySelector(
+      `${this.sliderRoot} .caption`
+    ).innerText = `${this.slideIndex}/${images.length}`;
   }
 
-  if (slideIndex === 1) {
-    console.log("disable prev");
-    document.getElementById("prev").disabled = true;
-  } else {
-    document.getElementById("prev").disabled = false;
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
   }
 
-  if (nextSlide > slides.length) {
-    slideIndex = 1;
-  }
-  if (nextSlide < 1) {
-    slideIndex = slides.length;
-  }
+  constructor(sliderRoot, images) {
+    this.images = images;
+    this.sliderRoot = sliderRoot;
+    document.querySelector(sliderRoot).innerHTML = getSliderString(
+      images[0],
+      images.length
+    );
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    document
+      .querySelector(`${sliderRoot} .prev`)
+      .addEventListener("click", () => {
+        this.plusSlides(-1);
+      });
+
+    document
+      .querySelector(`${sliderRoot} .next`)
+      .addEventListener("click", () => {
+        this.plusSlides(1);
+      });
   }
-  slides[slideIndex - 1].style.display = "block";
 }
+
+const images = [
+  "https://media.istockphoto.com/photos/funny-west-highland-white-terrier-dog-decorated-with-photo-props-sits-picture-id1292884801?b=1&k=20&m=1292884801&s=170667a&w=0&h=L5QgEFpFN1be2Qx8Q9PUWolafU_ecaqYiNwga6eoqxs=",
+  "https://media.istockphoto.com/photos/dog-jumping-in-autumn-picture-id1280392364?b=1&k=20&m=1280392364&s=170667a&w=0&h=NGYg-_9YcmJt2vkj79MvNxEcUTMN66Pa0KfvZ5MXVjI=",
+  "https://media.istockphoto.com/photos/young-man-is-playing-with-a-dog-and-do-selfie-picture-id1300658241?b=1&k=20&m=1300658241&s=170667a&w=0&h=0lrTViinfnDjbWDgxV0TDDSAXvzSgmrN-pKq0q60hqA=",
+  "https://media.istockphoto.com/photos/funny-dog-desires-to-eat-apple-pie-made-for-thanksgiving-dinner-picture-id1341269203?b=1&k=20&m=1341269203&s=170667a&w=0&h=5YRNyBVGh6XPe0MFp4ToxBY7amtt9CMzcCj6K9vsfEA="
+];
+
+const slider1 = new Slider("#slider1", images);
